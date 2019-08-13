f11bb8b578e0c2e72f15ca1f4fd1163ab7d6f8a9
ganderzz <stolen3@gmail.com>
2019-08-12 21:40:24 -0700

    cleanup and add width/height

--- src/img_reader.nim
+++ src/img_reader.nim
@@ -1,10 +1,10 @@
-import strutils
+import strutils, sequtils, sugar
 
 type
   FormatType = enum
-    ftBitMap  # P1
-    ftGrayMap # P2
-    ftPixMap  # P3
+    ftBitMap = "P1"
+    ftGrayMap = "P2"
+    ftPixMap = "P3"
 
   Image = ref object
     width: uint16
@@ -18,32 +18,32 @@ type
     of ftPixMap:
       maxColorValue: uint16
 
-proc getFormatFromString(input: string): FormatType =
-  ## Figures out the format of the image based.
-  if input == "P1":
-    return FormatType.ftBitMap
-  if input == "P2":
-    return FormatType.ftGrayMap
-  if input == "P3":
-    return FormatType.ftPixmap
-
-  raise newException(ObjectConversionError,
-      "Could not get image format from $# (looking for: P1, P2, P3). " % [input])
+method `$`(img: Image): string {.base.} =
+  return "Image [$#]: $#x$#" % [$img.format, $img.width, $img.height]
 
 proc fileToImage(input: TaintedString): Image =
-  ## Converts file input (as string) to an Image object
-  let splitLines = input.splitLines()
+  ## Converts file input (as string) to an Image object.
+
+  # split input by lines, and remove any comments
+  let splitLines = input.splitLines().filter(p => not p.startsWith('#'))
 
   for index in 0..splitLines.high:
     let row = splitLines[index]
 
     if index == 0:
-      result = Image(format: getFormatFromString(row))
+      # first line should be the image format type
+      result = Image(format: parseEnum[FormatType](row))
 
+    if index == 1:
+      # second line should be the image width x height
+      let splitRow = row.split(' ')
+
+      result.width = (uint16)parseUInt(splitRow[0])
+      result.height = (uint16)parseUInt(splitRow[1])
 
 when isMainModule:
   var fileContents = readFile("./test.pgm")
 
   let img = fileToImage(fileContents)
-  echo img.format
+  echo $img
 
