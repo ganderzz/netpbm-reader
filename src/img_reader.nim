81eaef12cf3e00afb142f09736b7ae56b11563f0
ganderzz <stolen3@gmail.com>
2019-09-17 05:52:46 -0700

    cleanup

--- src/img_reader.nim
+++ src/img_reader.nim
@@ -1,4 +1,4 @@
-import strutils, sequtils, sugar
+import strutils, sequtils, sugar, options
 
 type
   FormatType = enum
@@ -7,43 +7,64 @@ type
     ftPixMap = "P3"
 
   Image = ref object
-    width: uint16
-    height: uint16
-    data: seq[seq[uint16]]
+    width: int
+    height: int
+    data: seq[seq[int]]
     case format: FormatType
     of ftBitMap:
       discard
     of ftGrayMap:
-      maxWhiteColor: uint16
+      maxWhiteColor: int
     of ftPixMap:
-      maxColorValue: uint16
+      maxColorValue: int
 
 method `$`(img: Image): string {.base.} =
   return "Image [$#]: $#x$#" % [$img.format, $img.width, $img.height]
 
-proc fileToImage(input: TaintedString): Image =
+method readDimensions(img: Image, line: string): void {.base.} =
+  let splitRow = line.splitWhitespace()
+
+  img.width = parseInt(splitRow[0])
+  img.height = parseInt(splitRow[1])
+
+method readMaxColorValue(img: Image, line: string): void {.base.} =
+  if img.format == FormatType.ftGrayMap:
+    img.maxWhiteColor = parseInt(line)
+  if img.format == FormatType.ftPixMap:
+    img.maxColorValue = parseInt(line)
+
+proc fileToImage*(input: TaintedString): Image =
   ## Converts file input (as string) to an Image object.
 
   # split input by lines, and remove any comments
   let splitLines = input.splitLines().filter(p => not p.startsWith('#'))
 
-  for index in 0..splitLines.high:
+  if splitLines.len() < 2:
+    raise newException(ValueError, "Invalid image header length.")
+
+  result = Image(format: parseEnum[FormatType](splitLines[0]))
+
+  # second line should be the image width x height
+  result.readDimensions(splitLines[1])
+  result.data = newSeqWith(result.width, newSeq[int](result.height))
+
+  # third line is max color value for p2 and p3
+  result.readMaxColorValue(splitLines[2])
+
+  for index in 3..splitLines.high:
     let row = splitLines[index]
+    let splitRow = row.splitWhitespace()
 
-    if index == 0:
-      # first line should be the image format type
-      result = Image(format: parseEnum[FormatType](row))
+    if len(splitRow) > result.width:
+      raise newException(ValueError, "Row [$#] has an invalid width $#, expecting [$#]" %
+          [$index, $len(splitRow), $result.width])
 
-    if index == 1:
-      # second line should be the image width x height
-      let splitRow = row.split(' ')
-
-      result.width = (uint16)parseUInt(splitRow[0])
-      result.height = (uint16)parseUInt(splitRow[1])
+    result.data[index - 3].add(splitRow.map(parseInt))
 
 when isMainModule:
   var fileContents = readFile("./test.pgm")
 
   let img = fileToImage(fileContents)
   echo $img
+  echo $img.data
 
